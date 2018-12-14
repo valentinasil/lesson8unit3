2a01de036ea658a9262c187c911d4ef01fe73e40
valentinasil <43757543+valentinasil@users.noreply.github.com>
2018-12-14 10:08:17 -0500

    Add files via upload

---
+++ Lesson 8/problem1.py
@@ -0,0 +1,16 @@
+print('-'*65)
+print('Morbib Predictions Program:')
+print()
+print('descripton: This program ask for your current age and tells you the year that you will die (on avarage).')
+print()
+
+age=input('what is your age today? ')
+age=int(age)
+print('...thinking...')
+print('...thinking...')
+print('...thinking...')
+
+year= (2018-age)+79
+year=str(year)
+year=('you will die in the year...' +year)
+print(year)
